51bf6c64f72780def849c8b2cf380c0ad6d826e0
Michal Pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2021-04-24 11:28:16 +0200

    jsloading

--- baza/js/menu.js
+++ baza/js/menu.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function (){
+setTimeout(5000)
+document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('#navbar');
     function addShadow() {
         if (window.scrollY >= 150){
